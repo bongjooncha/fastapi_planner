9ea963edc45447b9a6158d665e548cb89b7a44a1
hiddenbebb <cbj4133@naver.com>
2024-09-05 23:15:17 +0900

    mysql delete 제외 완료

--- back/main.py
+++ back/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from routes import user_router, event_router
+from database.connections import conn
 
 app = FastAPI()
 app.add_middleware(
@@ -10,6 +12,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.on_event("startup")
+async def home():
+    return RedirectResponse(url="/event")
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
--- back/routes/events.py
+++ back/routes/events.py
@@ -1,54 +1,56 @@
-from fastapi import APIRouter, HTTPException, status, Body
-from models.events import Event
-from typing import List
-
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from database.connections import get_session
+from sqlmodel import select
+from models.events import *
 
 event_router = APIRouter(
     tags=["Events"]
 )
 
-events = []
+# 생성
+@event_router.post("/new")
+async def create_event(new_event: Event,
+                       session=Depends(get_session)) -> dict:
+    session.add(new_event)
+    session.commit()
+    session.refresh()
 
-@event_router.get("", response_model=List[Event])
-async def retrieve_all_events() -> List[Event]:
-    return events
-
-@event_router.get("/{id}", response_model=Event)
-async def retrive_event(id: int) -> Event:
-    for event in events:
-        if event.id == id:
-            return event
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Event with supplied ID doesn't exist"
-    )
-
-@event_router.put("/new")
-async def create_event(body: Event = Body(...)) -> dict:
-    events.append(body)
     return{
-        "message": "Event created successfully"
+        "message": "Event created succsessfully"
     }
 
+# 읽어오기
+@event_router.get("/", response_model= List[Event])
+async def retrieve_all_events(session = Depends(get_session)) -> List[Event]:
+    statement = select(Event)
+    events = session.exec(statement).all()
+    return events
 
-@event_router.delete("/{id}")
-async def delete_event(id: int) -> dict:
-    for event in events:
-        if event.id == id:
-            events.remove(event)
-            return{
-                "message": "Event deleted successfully"
-            }
-    
+# 특정부분 읽어오기
+@event_router.get("/{id}", response_model=Event)
+async def retrieve_event(id: int, session = Depends(get_session))-> Event:
+    event = session.get(Event,id)
+    if event:
+        return event
     raise HTTPException(
-        status_code= status.HTTP_404_NOT_FOUND,
+        status_code=status.HTTP_404_NOT_FOUND,
         detail="Event with supplied ID does not exist"
     )
 
+@event_router.put("/edit/{id}")
+async def update_event(id: int, new_data: EventUpdate, session = Depends(get_session)) -> Event:
+    event = session.get(Event,id)
+    if event:
+        event_data = new_data.dict(exclude_unset=True)
+        for key,value in event_data.items():
+            setattr(event, key, value)
+        session.add(event)
+        session.commit()
+        session.refresh(event)
 
-@event_router.delete("/")
-async def delete_all_events() -> dict:
-    events.clear()
-    return {
-        "message": "Events deleted successfully"
-    }
+        return event
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist"
+    )
+
